411485689aa6d18664c1bcbd81a9dc8c5643d474
clr <clr@port49.com>
2009-07-17 18:44:44 -0400

    Initial commit to nested_tree_comparisons.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'nested_tree_comparisons'
+
+Spec::Runner.configure do |config|
+  
+end
